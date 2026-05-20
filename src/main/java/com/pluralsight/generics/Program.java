014926af8ed00198dbd6b438932efeed252a109b
Hurera Ranjha <hurera@swiftcontrol.systems>
2026-05-20 13:01:23 -0400

    Finished Exercise and Bonus

--- src/main/java/com/pluralsight/generics/Program.java
+++ src/main/java/com/pluralsight/generics/Program.java
@@ -1,9 +1,24 @@
 package com.pluralsight.generics;
 
-public class Main
+public class Program
 {
     public static void main(String[] args)
     {
-        
+        SwapBox<String> words = new SwapBox<>("coffee", "tea");
+        SwapBox<Integer> scores = new SwapBox<>(85, 92);
+        SwapBox<Double> prices = new SwapBox<>(4.99, 9.49);
+
+        displayBox("Words", words);
+        displayBox("Scores", scores);
+        displayBox("Prices", prices);
+
+
+    }
+
+    public static <T> void displayBox(String label, SwapBox<T> box)
+    {
+        System.out.println(label + "before swap: " + box.describe());
+        box.swap();
+        System.out.println(label + "after swap: " + box.describe() + "\n");
     }
 }
--- src/main/java/com/pluralsight/generics/SwapBox.java
+++ src/main/java/com/pluralsight/generics/SwapBox.java
@@ -1,4 +1,51 @@
 package com.pluralsight.generics;
 
-public class SwapBox {
+public class SwapBox<T>
+{
+    private T leftItem;
+    private T rightItem;
+
+    public SwapBox(T leftItem, T rightItem)
+    {
+        this.leftItem = leftItem;
+        this.rightItem = rightItem;
+    }
+
+    public T getLeftItem()
+    {
+        return leftItem;
+    }
+
+    public void setLeftItem(T leftItem)
+    {
+        this.leftItem = leftItem;
+    }
+
+    public T getRightItem()
+    {
+        return rightItem;
+    }
+
+    public void setRightItem(T rightItem)
+    {
+        this.rightItem = rightItem;
+    }
+
+    public void swap()
+    {
+        T temp = this.leftItem;
+        this.leftItem = rightItem;
+        this.rightItem = temp;
+    }
+
+    public String describe()
+    {
+        return "Left: " + leftItem + ", Right: " + rightItem;
+    }
+
+    public boolean itemsMatch()
+    {
+        return this.leftItem.equals(this.rightItem);
+    }
+
 }
